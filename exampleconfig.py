5b06c6f93f835cd68121347cdf446dc5a598ef8a
salemhsh <147612290+salemhsh@users.noreply.github.com>
2023-10-11 14:24:12 +0200

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -3,13 +3,69 @@ from sample_config import Config
 
 class Development(Config):
     # ضـع اكـوادك تحت مكـان الكـلام العـربي  .. امسـح الكلام العربي اللي بين " " فقـط وضع اكـوادك
-    APP_ID = 6 #ضع كود الايبي ايدي الصغيـر مكان الرقم 6 نسخ لصق   
-    API_HASH = "ضع كود الايبي هاش"
-    ALIVE_NAME = "اسم حسابك التلي"
-    DB_URI = "رابـط التخـزين الخـاص بك"
-    STRING_SESSION = "كود تيرمــكس"
-    TG_BOT_TOKEN = "توكـن البـوت الخـاص بك"
-    PRIVATE_GROUP_BOT_API_ID = -100  # ضـع ايـدي كـروب السجـل بجـانب العـدد -100 لا تمسـح العـدد هـذا
+    APP_ID = 27097394 #ضع كود الايبي ايدي الصغيـر مكان الرقم 6 نسخ لصق   
+    API_HASH = "043cbace515884c8316b19ecfc609b8a"
+    ALIVE_NAME = "salem"
+    DB_URI = "postgres://ggwdhzii:hNAVxMw5YetTJsmGPI0_mVr316V90-Cm@flora.db.elephantsql.com/ggwdhzii"
+    STRING_SESSION = "1BJWap1wBu2zEFL5Zlfgms3pH1GHFES49__3q3cyjahZ9jY3lvIZnkfAjU95E6GX7FjpeXZCnxVagZGu195p2i3nFzrjdHzmOqXABkSA_tNHdOZSYfG9zh2lz3vDV4H0OAm6FTM47ee5I4tU35JoApLJk0FtmmRG20DKFIbd1wd5ffOt1b-WdHBNzzTyREJOfWst_f5SS7QsS2GUhNiK5m4c5SWM7hfuhVbCl1yLpmEUeoT0UmWY_mw3v7l-ru-s94P6Djk8xeNwuNvVTQSGfvqbVkZx1WFV_N7us6W2SkRahSG9EvbgPUoKGN9vPR2msoGYOry7p9aRPLMBbkbtTFiKUOwCw0Kk="
+    TG_BOT_TOKEN = "6672019617:AAFQSzs3TipyskPN8By_60Sg_47BvnyJOgc"
+    PRIVATE_GROUP_BOT_API_ID = -1001681122495 # ضـع ايـدي كـروب السجـل بجـانب العـدد -100 لا تمسـح العـدد هـذا
     COMMAND_HAND_LER = "."  # اتركهــا كمـا هـي
     SUDO_COMMAND_HAND_LER = "."  # اتركهــا كمـا هـي
-    TZ = "Asia/Baghdad"  # اتركهــا كمـا هـي
+    TZ = "Africa/tripoli"  # اتركهــا كمـا هـي
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
